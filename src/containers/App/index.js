4bd011027f73f506e92a60f2ac660b78cb6de2e9
Jonathanfilbert <jonathan.lisyanto@bpkpenabur.sch.id>
2019-08-22 15:23:34 +0700

    simple SEO using helmet

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,20 +1,24 @@
 import React from "react";
 import LandingPage from "../../components/LandingPage";
 import NavBar from "../../components/NavBar";
+import { Helmet } from "react-helmet";
 
 import { AppContainer } from "./style";
 
-
-class App extends React.Component {
-  render() {
-    return (
-      <AppContainer>
-        <NavBar/>
-        <LandingPage />
-      </AppContainer>
-    );
-  }
-}
+const App = () => (
+  <AppContainer>
+    <Helmet>
+      <title>LYTEBOX - All in one web-based multimedia suite </title>
+      <link rel="canonical" href="http://lytebox.studio" />
+      <meta
+        name="description"
+        content="Lytebox is the world's first web-based all in one multimedia suite."
+      />
+    </Helmet>
+    <NavBar />
+    <LandingPage />
+  </AppContainer>
+);
 
 App.propTypes = {};
 
